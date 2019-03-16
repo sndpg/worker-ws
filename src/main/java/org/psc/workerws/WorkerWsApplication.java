0fd69fadf8c6175cf0185b0e3c5ffb3eed1ce68e
sykq <genericmailacc@gmail.com>
2019-03-16 10:06:26 +0100

    Initial commit

---
+++ src/main/java/org/psc/workerws/WorkerWsApplication.java
@@ -0,0 +1,13 @@
+package org.psc.workerws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerWsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/psc/workerws/WorkerWsRestController.java
@@ -0,0 +1,46 @@
+package org.psc.workerws;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.security.SecureRandom;
+import java.util.stream.Stream;
+
+@RestController("/service")
+public class WorkerWsRestController {
+
+    @GetMapping("/status")
+    public ResponseEntity<String> getStatus() {
+
+        return new ResponseEntity<>("{\"status\":\"OK\"}", HttpStatus.OK);
+    }
+
+    @GetMapping("/randomNumber")
+    public Double getRandomNumber() {
+        return new SecureRandom().nextDouble();
+    }
+
+    @GetMapping("/functional/mono/randomNumber")
+    public Mono<Double> getRandomNumberFunctionallyAsMono() {
+        return Mono.fromSupplier(() -> new SecureRandom().nextDouble());
+    }
+
+    @GetMapping(value = "/functional/flux/randomNumber", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Double> getRandomNumberFunctionallyAsFlux() {
+        return Flux.fromStream(Stream.generate(() -> new SecureRandom().nextDouble()));
+    }
+
+
+    @GetMapping("/flux/randomNumber")
+    public ResponseEntity<Flux<Double>> getRandomNumberAsFlux() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Flux.fromStream(Stream.generate(() -> new SecureRandom().nextDouble()).limit(500)));
+    }
+}
