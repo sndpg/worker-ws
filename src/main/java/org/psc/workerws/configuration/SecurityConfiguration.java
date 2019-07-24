421f821ebcaa6957b92827bbc4cace3a2d02d577
sykq <genericmailacc@gmail.com>
2019-07-24 20:38:29 +0200

    basic auth ldpa

--- src/main/java/org/psc/workerws/configuration/SecurityConfiguration.java
+++ src/main/java/org/psc/workerws/configuration/SecurityConfiguration.java
@@ -2,23 +2,17 @@ package org.psc.workerws.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("sykq")
-                .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("password"))
-                .authorities("ROLE_USER");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,6 +23,40 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .httpBasic();
+    }
+
+    @Profile("ldap")
+    @Configuration
+    static class LdapAuthenticationConfiguration {
+
+        @Autowired
+        public void configureLdapAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+            auth.ldapAuthentication()
+                    .userDnPatterns("uid={0},ou=people")
+                    .groupSearchBase("ou=groups")
+                    .contextSource()
+                    //.url("ldap://localhost:8389/dc=springframework,dc=org")
+                    .ldif("classpath:ldap-server.ldif")
+                    .and()
+                    .passwordCompare()
+                    .passwordEncoder(new LdapShaPasswordEncoder())
+                    .passwordAttribute("userPassword");
+        }
+
+    }
+
+    @Profile("!ldap")
+    @Configuration
+    static class InMemoryAuthenticationConfiguration {
+
+        @Autowired
+        public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("sykq")
+                    .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("password"))
+                    .authorities("ROLE_USER");
+        }
+
     }
 
 }
